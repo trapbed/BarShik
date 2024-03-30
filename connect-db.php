254e2a12d81c2a3c7064648dd11f5622a0bb2936
trapbed <148848875+trapbed@users.noreply.github.com>
2024-03-30 10:42:12 +0500

    верстка 1

---
+++ connect-db.php
@@ -0,0 +1,3 @@
+<?php
+    $con = mysqli_connect("localhost", "root", "", "bar_shik");
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHIK</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id='backgroundIndex'>
+        <div id='fizzBack'>
+            <span class='fizzBackWord'>fizz</span>
+            <span class='fizzBackWord'>fizz</span>
+            <span class='fizzBackWord'>fizz</span>
+            <!-- <span class='fizzBackWord'>fizz</span> -->
+            <!-- <span class='fizzBackWord'>fizz</span> -->
+            <span class='fizzBackWord'>fizz</span>
+        </div>
+        <img src="images/bank.png" alt="energy bank">
+        <div id='shadowCircle'></div>
+    </div>
+</body>
+</html>
